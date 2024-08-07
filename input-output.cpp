ebeaa6e897afcbfdcb0fec0c5c269ca8a6c42da4
slvester29 <nemesiscrisis49@gmail.com>
2024-08-07 12:00:15 +0700

    Add files via upload

---
+++ input-output.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	char k;
+
+
+	cout << "========BERIKUT DATA IDENTITAS=============" << endl;
+	cout << "\n klik n untuk melanjutkan \n" << endl;
+
+
+	cout << "nomor induk siswa = ";
+	cout << 124354578 << endl;
+	cout << " Nama_Siswa = satriani" << endl;
+	cout << "kelas = Teknik 11.2" << endl;
+	cout << "jurusan = teknik" << endl;
+	cout << "l" << endl;
+
+	cin >> k;
+
+	cout << "nomor induk siswa = ";
+	cout << 11111111 << endl;
+	cout << " Nama_Siswa = Shahmmrel" << endl;
+	cout << "kelas = Teknik 11.2" << endl;
+	cout << "jurusan = teknik" << endl;
+	cout << "l" << endl;
+
+	cin >> k;
+
+	cout << "nomor induk siswa = ";
+	cout << 18373737 << endl;
+	cout << " Nama_Siswa = samuel" << endl;
+	cout << "kelas = Teknik 11.2" << endl;
+	cout << "jurusan = teknik" << endl;
+	cout << "l" << endl;
+	
+	cin >> k;
+
+
+	cout << "nomor induk siswa = ";
+	cout << 124354578 << endl;
+	cout << " Nama_Siswa = Slvester Satriani Gunarsa" << endl;
+	cout << "kelas = Teknik 11.2" << endl;
+	cout << "jurusan = teknik" << endl;
+	cout << "l" << endl;
+
+	cin >> k;
+		
+	cout << "nomor induk siswa = ";
+	cout << 66689480 << endl;
+	cout << " Nama_Siswa = alberto" << endl;
+	cout << "kelas = Teknik 11.2" << endl;
+	cout << "jurusan = teknik" << endl;
+	cout << "l" << endl;
+
+
+
+
+
+
+
+
+	
+
+
+
+	return 0;
+
+
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+	char n;
+	char s;
+	char m;
+	char f;
+
+
+	cout << "==============OUTPUT================ \n" << endl;
+	cout << "INSTRUKSI UNTUK MELANJUTKAN ketik n karakter alphabet \n" << endl;
+
+
+
+	cout << "judul Nama-siswa" << endl;
+	cout << "nama: Slvester Satriani Gunarsa" << endl;
+	cout << "asal kota kelahiran= Karawang" << endl;
+	cout << "nomor absen= 25" << endl;
+	cout << "cita-cita= Fisikawan" << endl;
+	
+	cout << "============KLIK UNTUK MELANJUTKAN" << endl;
+
+
+	cin >> n;
+
+	cout << "judul Nama-siswa" << endl;
+	cout << "nama: Alberto Maruli" << endl;
+	cout << "asal kota kelahiran= Karawang" << endl;
+	cout << "nomor absen= 2" << endl;
+	cout << "cita-cita= Fisikawan" << endl;
+
+
+	cin  >> s;
+	
+	cout << "judul Nama-siswa" << endl;
+	cout << "nama: Shamarel" << endl;
+	cout << "asal kota kelahiran= Karawang" << endl;
+	cout << "nomor absen= 24" << endl;
+	cout << "cita-cita= Akpol" << endl;
+	cout << "judul Nama-siswa" << endl;
+	
+	cin >> m;
+
+	cout << "judul Nama-siswa" << endl;
+	cout << "nama: Samuel Nugraha" << endl;
+	cout << "asal kota kelahiran= Karawang" << endl;
+	cout << "nomor absen= 23" << endl;
+	cout << "cita-cita= Akpol" << endl;
+	
+
+
+
+	return 0;
+	
+
+
+
+
+
+
+
+}
